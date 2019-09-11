6a7c2ef4aba5f49ce81913f97e6eec0d6978a551
9240 <z20159240@gmail.com>
2019-09-11 18:37:55 +0800

    后台

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 var db = require('../db/index')
 
 router.get('/', function(req, res, next) {
-  db.connection.query(`select * from zn_news limit 0,3`, function (error, results, fields) {
+  db.connection.query(`select * from zn_news limit 0,3`, function (error, resNews, fields) {
     if (error) throw error;
-    res.render('index/index',{newsList:results});
+    db.connection.query(`select * from zn_carousel`, function (error, resCarousel, fields) {
+      if (error) throw error;
+      console.log(resCarousel)
+      res.render('index/index',{newsList:resNews,carousel:resCarousel});
+    });
   });
 });
 
