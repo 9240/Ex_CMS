88f802980aa5c4d16334922e47edfa6d91eaab0c
9240 <z20159240@gmail.com>
2019-09-11 18:47:01 +0800

    合并分支

--- routes/admin.js
+++ routes/admin.js
@@ -34,12 +34,44 @@ router.all("*",(req,res,next)=>{
 })
 
 router.get('/', function(req, res, next) {
-  db.connection.query(`select * from zn_category`,function (error, results) {
-    if (error) throw error;
-    res.render('admin/index',{proCate:results});
-  });
+  res.render('admin/index');
 });
 
+router.get('/carousel', function(req, res, next) {
+  db.connection.query(`select * from zn_carousel`,function (error, results) {
+    if (error) throw error;
+    res.render('admin/pages/carousel',{carousel:results});
+  });
+});
+router.get('/proCate', function(req, res, next) {
+  db.connection.query(`select * from zn_category`,function (error, results) {
+    if (error) throw error;
+    res.render('admin/pages/proCate',{proCate:results});
+  });
+});
+router.get('/product', function(req, res, next) {
+  db.connection.query(`select * from zn_pro`,function (error, resultsPro) {
+    if (error) throw error;
+    db.connection.query(`select * from zn_category`,function (error, resultsCate) {
+      if (error) throw error;
+      for(var i in resultsPro){
+        for(var j in resultsCate){
+          if(resultsPro[i].cate_id == resultsCate[j].id){
+            resultsPro[i]["cateName"] = resultsCate[j].category
+          }
+        }
+      }
+      console.log(resultsPro)
+      res.render('admin/pages/product',{product:resultsPro,proCate:resultsCate});
+    });
+  });
+});
+router.get('/news', function(req, res, next) {
+  db.connection.query(`select * from zn_news`,function (error, results) {
+    if (error) throw error;
+    res.render('admin/pages/news',{newsList:results});
+  });
+});
 router.get('/login', function(req, res, next) {
   res.render('admin/login');
 });
@@ -65,15 +97,60 @@ router.post('/login',(req,res,next)=>{
 })
 
 
-//——————————————————————————————————————————添加新闻————————————————————————————————————————
+//——————————————————————————————————————————添加轮播图————————————————————————————————————————
 //单个文件上传
+router.post('/addCarousel',upload.single('img'), (req,res)=>{
+  db.connection.query(`insert into zn_carousel values (null,'/files/${req.file.filename}')`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
+    })
+  });
+});
+//——————————————————————————————————————————删除轮播图————————————————————————————————————————
+//单个文件上传
+router.post('/delCarousel',(req,res)=>{
+  db.connection.query(`delete from zn_carousel where id = ${req.body.id}`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
+    })
+  });
+});
+//——————————————————————————————————————————添加新闻————————————————————————————————————————
 router.post('/addNews',upload.array('img'), (req,res)=>{
   var news = JSON.parse(req.body.news)
-  db.connection.query(`insert into zn_news values (null,'${news.title}','${news.date}','files/${req.files[0].filename}','${news.newsContent}')`,function (error, results) {
+  db.connection.query(`insert into zn_news values (null,'${news.title}','${news.date}','/files/${req.files[0].filename}','${news.newsContent}')`,function (error, results) {
     if (error) throw error;
     res.json({
       code:200,
       msg:"添加成功"
+    })
+  });
+});
+//——————————————————————————————————————————删除新闻————————————————————————————————————————
+router.post('/delNews',upload.array('img'), (req,res)=>{
+  db.connection.query(`delete from zn_news where id = ${req.body.id}`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
+      msg:"添加成功"
+    })
+  });
+});
+//——————————————————————————————————————————编辑新闻————————————————————————————————————————
+router.post('/editNews',upload.array('img'), (req,res)=>{
+  var news = JSON.parse(req.body.news)
+  var sql;
+  if(req.files.length == 0){
+    sql = `update zn_news set title='${news.title}' , date='${news.date}' , Content='${news.newsContent}' where id=${news.id}`
+  }else{
+    sql = `update zn_news set title='${news.title}' , date='${news.date}' , Content='${news.newsContent}' , img='/files/${req.files[0].filename}' where id=${news.id}`
+  }
+  db.connection.query(sql,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
     })
   });
 });
@@ -93,14 +170,55 @@ router.post('/addCate',(req,res,next)=>{
 //单个文件上传
 router.post('/addPro',upload.array('proImg'), (req,res)=>{
   var pro = JSON.parse(req.body.pro)
-  console.log(pro)
-  db.connection.query(`insert into zn_pro values (null,'${pro.proCate}','${pro.proTitle}','${pro.proContent}','files/${req.files[0].filename}')`,function (error, results) {
+  db.connection.query(`insert into zn_pro values (null,'${pro.proCate}','${pro.proTitle}','${pro.proContent}','/files/${req.files[0].filename}')`,function (error, results) {
     if (error) throw error;
-    console.log(results);
     res.json({
       code:200,
       msg:"添加成功"
     })
   });
 });
+//——————————————————————————————————————————编辑产品————————————————————————————————————————
+router.post('/editPro',upload.array('proImg'), (req,res)=>{
+  var pro = JSON.parse(req.body.pro)
+  var sql;
+  if(req.files.length == 0){
+    sql = `update zn_pro set cate_id='${pro.proCate}' , proName='${pro.proTitle}' , proDesc='${pro.proContent}' where id=${pro.id}`
+  }else{
+    sql = `update zn_pro set cate_id='${pro.proCate}' , proName='${pro.proTitle}' , proDesc='${pro.proContent}' , proImg='/files/${req.files[0].filename}' where id=${pro.id}`
+  }
+  db.connection.query(sql,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
+    })
+  });
+});
+//——————————————————————————————————————————删除产品————————————————————————————————————————
+router.post('/delPro', (req,res,next)=>{
+  db.connection.query(`delete from zn_pro where id=${req.body.id}`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200,
+    })
+  });
+});
+//——————————————————————————————————————————编辑分类————————————————————————————————————————
+router.post('/editCate',(req,res,next)=>{
+  db.connection.query(`update zn_category set category = '${req.body.category}' where id = ${req.body.id}`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200
+    })
+  });
+})
+//——————————————————————————————————————————删除分类————————————————————————————————————————
+router.post('/delCate',(req,res,next)=>{
+  db.connection.query(`delete from zn_category where id = ${req.body.id}`,function (error, results) {
+    if (error) throw error;
+    res.json({
+      code:200
+    })
+  });
+})
 module.exports = router;
